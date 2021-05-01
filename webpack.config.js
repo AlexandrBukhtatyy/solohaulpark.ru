263329555502b7b0e363d76c6e64646bdc966ae3
Alexandr Bukhtatyy <buhtatyalexander90@gmail.com>
2021-05-01 22:06:13 +0300

    адаптив футера

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: "hello webpack",
-            template: "./src/404.html",
+            template: "./src/index.html",
             // chunks: ['main']
         }),
         new CopyPlugin({
