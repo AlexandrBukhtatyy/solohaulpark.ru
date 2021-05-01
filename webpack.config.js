83a438f1a5c101a815873ff812c5bc05b2527b43
Alexandr Bukhtatyy <buhtatyalexander90@gmail.com>
2021-05-01 21:12:44 +0300

    добавил вебпак - для генерации спрайтов svg

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
-const path = require('path')
+const path = require('path');
+const webpack = require('webpack');
 const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, './dist'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
     entry: {
         main: path.resolve(__dirname, './src/assets/js/index.js'),
     },
@@ -13,25 +24,38 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/index.html"
+            title: "hello webpack",
+            template: "./src/404.html",
+            // chunks: ['main']
         }),
         new CopyPlugin({
             patterns: [
                 {
-                    from: "src/assets/fonts/*",
+                    from: "src/assets/css",
+                    to: "assets/css",
+                },
+                {
+                    from: "src/assets/fonts",
                     to: "assets/fonts",
                 },
                 {
-                    from: "src/assets/img/*",
+                    from: "src/assets/img",
                     to: "assets/img",
-                },
-                {
-                    from: "src/assets/svg/*",
-                    to: "assets/svg",
                 },
             ],
         }),
-        new CleanWebpackPlugin()
+        new SVGSpritemapPlugin({
+            sprite: {
+                prefix: false,
+            },
+            output: {
+                filename: '/assets/svg/sprite.svg',
+                svg4everybody: true,
+                svgo: true,
+            }
+        }),
+        new CleanWebpackPlugin(),
+        // new webpack.HotModuleReplacementPlugin(),
     ],
     module: {
         rules: [
@@ -42,5 +66,6 @@ module.exports = {
                 use: ['babel-loader'],
             },
         ],
+
     }
 }
