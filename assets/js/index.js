bf7a6f8d120e8df3594c7e87f471bcc3a05cf149
abukhtatyy <abukhtatyy@gmail.com>
2021-05-08 14:40:56 +0300

    шапка на странице "О парке"

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,19 @@ $(document).ready(function(){
         $(this).toggleClass('open');
     });
 });
+
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 43.8071413, lng: 39.6113559 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("contact-map"), {
+        zoom: 10,
+        center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
